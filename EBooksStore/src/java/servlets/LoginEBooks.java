73114827619732fa18e9c94b62989757abe71b8b
Steven <Steven@Steven-PC>
2017-05-14 15:36:19 +0300

    isbn modify database

--- EBooksStore/src/java/servlets/LoginEBooks.java
+++ EBooksStore/src/java/servlets/LoginEBooks.java
@@ -173,13 +173,13 @@ public class LoginEBooks extends HttpServlet {
                             + "PRIMARY KEY (ISBN, ID))");
                     statement.execute("INSERT INTO ROOT.AUTHORS ("
                             + "ISBN, ID, FIRST_NAME, LAST_NAME) "
-                            + "VALUES ('321-54-54321-54', 1, 'Mihail', 'Sadoveanu')");
+                            + "VALUES ('321-54-54321-541', 1, 'Mihail', 'Sadoveanu')");
                     statement.execute("INSERT INTO ROOT.AUTHORS ("
                             + "ISBN, ID, FIRST_NAME, LAST_NAME) "
-                            + "VALUES ('999-12-12345-12', 2, 'Paul', 'Deitel')");
+                            + "VALUES ('999-12-12345-121', 2, 'Paul', 'Deitel')");
                     statement.execute("INSERT INTO ROOT.AUTHORS ("
                             + "ISBN, ID, FIRST_NAME, LAST_NAME) "
-                            + "VALUES ('999-12-12345-12', 3, 'Harvey', 'Deitel')");
+                            + "VALUES ('999-12-12345-121', 3, 'Harvey', 'Deitel')");
                     break;
                 case "EBOOKS" :
                     statement.execute("CREATE TABLE EBOOKS ("
@@ -192,13 +192,13 @@ public class LoginEBooks extends HttpServlet {
                             + "PRIMARY KEY (ISBN))");
                     statement.execute("INSERT INTO ROOT.EBOOKS (ISBN, TITLE, "
                             + "NUMBER_OF_PAGES, PRICE, RATING, \"TYPE\")"
-                            + " VALUES ('123-45-12345-12', 'Fratii Jderi', 792, 47.5, 4.0, 1)");
+                            + " VALUES ('123-45-12345-121', 'Fratii Jderi', 792, 47.5, 4.0, 1)");
                     statement.execute("INSERT INTO ROOT.EBOOKS (ISBN, TITLE, "
                             + "NUMBER_OF_PAGES, PRICE, RATING, \"TYPE\")"
-                            + " VALUES ('321-54-54321-54', 'Baltagul', 210, 22.0, 5.0, 1)");
+                            + " VALUES ('321-54-54321-541', 'Baltagul', 210, 22.0, 5.0, 1)");
                     statement.execute("INSERT INTO ROOT.EBOOKS (ISBN, TITLE, "
                             + "NUMBER_OF_PAGES, PRICE, RATING, \"TYPE\")"
-                            + " VALUES ('999-12-12345-12', 'Java, How to program', 1535, 100.0, 2.0, 2)");
+                            + " VALUES ('999-12-12345-121', 'Java, How to program', 1535, 100.0, 2.0, 2)");
                     break;
                 case "RATINGS" :
                     statement.execute("CREATE TABLE RATINGS ("
@@ -209,22 +209,22 @@ public class LoginEBooks extends HttpServlet {
                             + "PRIMARY KEY (ISBN, USER_NAME))");
                     statement.execute("INSERT INTO ROOT.RATINGS (ISBN, "
                             + "USER_NAME, DESCRIPTION, RATING) "
-                            + "	VALUES ('123-45-12345-12', 'User1', 'E buna', 5.0)");
+                            + "	VALUES ('123-45-12345-121', 'User1', 'E buna', 5.0)");
                     statement.execute("INSERT INTO ROOT.RATINGS (ISBN, "
                             + "USER_NAME, DESCRIPTION, RATING) "
-                            + "	VALUES ('123-45-12345-12', 'User2', 'E in regula', 3.0)");
+                            + "	VALUES ('123-45-12345-121', 'User2', 'E in regula', 3.0)");
                     statement.execute("INSERT INTO ROOT.RATINGS (ISBN, "
                             + "USER_NAME, DESCRIPTION, RATING) "
-                            + "	VALUES ('321-54-54321-54', 'User3', 'Cel mai bun roman', 5.0)");
+                            + "	VALUES ('321-54-54321-541', 'User3', 'Cel mai bun roman', 5.0)");
                     statement.execute("INSERT INTO ROOT.RATINGS (ISBN, "
                             + "USER_NAME, DESCRIPTION, RATING) "
-                            + "	VALUES ('999-12-12345-12', 'User1', 'Groaznic', 1.0)");
+                            + "	VALUES ('999-12-12345-121', 'User1', 'Groaznic', 1.0)");
                     statement.execute("INSERT INTO ROOT.RATINGS (ISBN, "
                             + "USER_NAME, DESCRIPTION, RATING) "
-                            + "	VALUES ('999-12-12345-12', 'User2', 'N-am inteles nimic', 2.0)");
+                            + "	VALUES ('999-12-12345-121', 'User2', 'N-am inteles nimic', 2.0)");
                     statement.execute("INSERT INTO ROOT.RATINGS (ISBN, "
                             + "USER_NAME, DESCRIPTION, RATING) "
-                            + "	VALUES ('999-12-12345-12', 'User4', 'Cat de cat ok', 3.0)");
+                            + "	VALUES ('999-12-12345-121', 'User4', 'Cat de cat ok', 3.0)");
                     break;
                 case "TYPES" :
                     statement.execute("CREATE TABLE TYPES ("
